333b675558db73ba9ad941af7db52e842110b08d
Vladyslav Rokhmaniuk <botaniqyes@gmail.com>
2024-01-04 13:49:43 +0200

    - add useRef to Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const input = useRef();
+  const [playerName, setPlayerName] = useState("unknown entity");
+
+  const handleClick = () => {
+    console.log("input", input);
+    setPlayerName(input.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={input} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
